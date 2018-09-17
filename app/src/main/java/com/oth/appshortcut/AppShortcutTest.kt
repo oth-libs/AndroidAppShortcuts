e2627ef16aa6cfbf3ce85edb199e8bf8ea7583a4
Othman EL JAZOULI <eljazouli.othman@gmail.com>
2018-09-17 11:29:54 +0200

    Initial commit

---
+++ app/src/main/java/com/oth/appshortcut/AppShortcutTest.kt
@@ -0,0 +1,5 @@
+package com.oth.appshortcut
+
+class AppShortcutTest{
+
+}
---
+++ sample/src/main/java/com/oth/appshortcut/sample/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.oth.appshortcut.sample
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.oth.appshortcut.AppShortcutTest
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val app : AppShortcutTest
+    }
+}
